73b990868fd4bcf54f21f5b8f67581a4180ad0f9
geok13 <32941134+geok13@users.noreply.github.com>
2018-04-12 16:19:19 +0300

    Add files via upload

---
+++ TwoTempSensorsonSerial.ino
@@ -0,0 +1,100 @@
+/* YourDuino Multiple DS18B20 Temperature Sensors on 1 wire
+  Connections:
+  DS18B20 Pinout for waterproof version
+  - Black cable   = Ground
+  - Yellow cable  = Signal (Pin 2):  (with a 4.7K resistor to +5 and Signal)
+  - Red cable  = +5 V
+
+   Questions: terry@yourduino.com 
+   V1.01  01/17/2013 ...based on examples from Rik Kretzinger
+   
+/*-----( Import needed libraries )-----*/
+// Get 1-wire Library here: http://www.pjrc.com/teensy/td_libs_OneWire.html
+#include <OneWire.h>
+
+//Get DallasTemperature Library here:  http://milesburton.com/Main_Page?title=Dallas_Temperature_Control_Library
+#include <DallasTemperature.h>
+
+/*-----( Declare Constants and Pin Numbers )-----*/
+#define ONE_WIRE_BUS_PIN 2
+
+/*-----( Declare objects )-----*/
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS_PIN);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+/*-----( Declare Variables )-----*/
+// Assign the addresses of your 1-Wire temp sensors.
+// See the tutorial on how to obtain these addresses:
+// http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
+
+DeviceAddress Probe01 = { 0x28, 0xFF, 0x06, 0x0E, 0x63, 0x16, 0x03, 0x024 }; 
+DeviceAddress Probe02 = { 0x28, 0xFF, 0xBC, 0xE5, 0x62, 0x16, 0x04, 0x23 };
+
+void setup()   /****** SETUP: RUNS ONCE ******/
+{
+  // start serial port to show results
+  Serial.begin(9600);
+  Serial.print("Initializing Temperature Control Library Version ");
+  Serial.println(DALLASTEMPLIBVERSION);
+  
+  // Initialize the Temperature measurement library
+  sensors.begin();
+  
+  // set the resolution to 10 bit (Can be 9 to 12 bits .. lower is faster)
+  sensors.setResolution(Probe01, 10);
+  sensors.setResolution(Probe02, 10);
+  //sensors.setResolution(Probe03, 10);
+  //sensors.setResolution(Probe04, 10);
+  //sensors.setResolution(Probe05, 10);
+
+}//--(end setup )---
+
+void loop()   /****** LOOP: RUNS CONSTANTLY ******/
+{
+  delay(1000);
+  Serial.println();
+  Serial.print("Number of Devices found on bus = ");  
+  Serial.println(sensors.getDeviceCount());   
+  Serial.print("Getting temperatures... ");  
+  Serial.println();   
+  
+  // Command all devices on bus to read temperature  
+  sensors.requestTemperatures();  
+  
+  Serial.print("Probe 01 temperature is:   ");
+  printTemperature(Probe01);
+  Serial.println();
+
+  Serial.print("Probe 02 temperature is:   ");
+  printTemperature(Probe02);
+  Serial.println();
+ 
+   delay(20000);   
+  
+}//--(end main loop )---
+
+/*-----( Declare User-written Functions )-----*/
+void printTemperature(DeviceAddress deviceAddress)
+{
+
+float tempC = sensors.getTempC(deviceAddress);
+
+   if (tempC == -127.00) 
+   {
+   Serial.print("Error getting temperature  ");
+   } 
+   else
+   {
+   Serial.print("C: ");
+   Serial.print(tempC);
+   //Serial.print(" F: ");
+   //Serial.print(DallasTemperature::toFahrenheit(tempC));
+   }
+}// End printTemperature
+//*********( THE END )***********
+
+
+
